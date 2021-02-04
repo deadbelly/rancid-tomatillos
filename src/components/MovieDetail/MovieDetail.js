ce0a8f5bab6b8305939d25c5d6b43b6334f21649
Thao Ma <MaThao@gmail.com>
2021-02-04 13:41:38 -0700

    Add display to movie detail page

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -3,15 +3,24 @@ import './MovieDetail.css';
 
 const MovieDetail = ({movie}) => {
   return (
-    <div>
-      {[
-        movie.id,
-        movie.title,
-        movie.average_rating,
-        movie.poster_path,
-        movie.release_date
-      ]}
-    </div>
+    <section>
+      <div className='detail-page-header'>
+        <h2>{movie.title}</h2>
+        <p>Genres: {movie.genres.map(genre => genre.name)}</p>
+      </div>
+      <div className='movie-info'>
+        <img src={movie.poster_path} />
+        <article>
+          <p>{movie.tagline}</p>
+          <p>{movie.overview}</p>
+        </article>
+        <article>
+          <p>Runtime: {movie.runtime} minutes</p>
+          <p>Budget: ${movie.budget}</p>
+          <p>Revenue: ${movie.revenue}</p>
+        </article>
+      </div>
+    </section>
   )
 }
 
