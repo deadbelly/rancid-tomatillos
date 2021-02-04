415dec4fa68eb0aa3b2cc660e16ea0c1e4cbd613
Boone Epstein <booneepstein@gmail.com>
2021-02-03 18:58:36 -0600

    Add styling to MovieList and Poster components

--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Poster.css'
 
 const Poster = ({id, title, rating, imgSrc}) => {
   return (
-    <div>
+    <div className="poster">
       <img src={imgSrc}/>
       <h2>{title}</h2>
-      <p>{rating.toFixed(1)}</p>
+      <p>{rating.toFixed(1)}/10 Fresh Tomatillos</p>
     </div>
   )
 }
