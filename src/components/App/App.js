fc6b4a40ac9bf8b505210adbaf95c52cae6f074f
Boone Epstein <booneepstein@gmail.com>
2021-02-04 16:12:41 -0600

    Add fetch call to get data from API

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,26 +9,33 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: movieData.movies,
+      movies: [],
       selectedId: 0
     };
   }
 
+  componentDidMount() {
+    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+      .then(response => response.json())
+      .then(data => this.setState({movies: data.movies}))
+  }
+
   selectMovie = id => {
+    //fetch movie from api with id, then assign that data to state.selected
     this.setState({selectedId: id})
   }
 
   render() {
     return (
       <div className="App">
-        <Header 
-        selectedId={this.state.selectedId} 
+        <Header
+        selectedId={this.state.selectedId}
         selectMovie={this.selectMovie}
         />
         <main>
-          {this.state.selectedId ? <MovieDetail movie={this.state.movies.find(movie => movie.id === this.state.selectedId)}/> : 
-            <MovieList 
-              movies={this.state.movies} 
+          {this.state.selectedId ? <MovieDetail movie={this.state.movies.find(movie => movie.id === this.state.selectedId)}/> :
+            <MovieList
+              movies={this.state.movies}
               selectMovie={this.selectMovie}
           />}
         </main>
