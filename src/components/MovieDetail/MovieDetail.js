8adf685268780d4eda680a2d00f0015e14d0a140
Thao Ma <MaThao@gmail.com>
2021-02-06 14:02:33 -0700

    Add styling to each widget box

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -3,18 +3,17 @@ import './MovieDetail.css';
 
 const MovieDetail = ({movie}) => {
   return (
-    <section>
-      <div className='detail-page-header'>
-        <h2>{movie.title}</h2>
-        <p>Genres: {movie.genres}</p>
-      </div>
-      <div className='movie-info'>
-        <img className='poster-image border' src={movie.backdrop_path} />
-        <article className='border'>
+    <section className='movie-detail-page'>
+      <div className='backdrop-img' style={{backgroundImage:`url(${movie.backdrop_path})`}}>
+        <article className='movie-info border'>
+          <div className='detail-page-header'>
+            <h2>{movie.title}</h2>
+            <p>Genres: {movie.genres}</p>
+          </div>
           <h3>{movie.tagline}</h3>
           <p className='overview'>{movie.overview}</p>
         </article>
-        <article className='border'>
+        <article className='movie-spec border'>
           <p>Runtime: {movie.runtime} minutes</p>
           <p>Budget: ${movie.budget}</p>
           <p>Revenue: ${movie.revenue}</p>
