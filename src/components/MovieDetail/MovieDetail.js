38872465a3c141e51c09108c011bd35496384eb8
Thao Ma <MaThao@gmail.com>
2021-02-06 14:37:09 -0700

    Add styiling to even out each widget box

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -5,18 +5,18 @@ const MovieDetail = ({movie}) => {
   return (
     <section className='movie-detail-page'>
       <div className='backdrop-img' style={{backgroundImage:`url(${movie.backdrop_path})`}}>
-        <article className='movie-info border'>
-          <div className='detail-page-header'>
-            <h2>{movie.title}</h2>
+        <article className='movie-spec movie-container border'>
+          <div>
+            <h2 className='movie-title'>{movie.title}</h2>
             <p>Genres: {movie.genres}</p>
           </div>
-          <h3>{movie.tagline}</h3>
-          <p className='overview'>{movie.overview}</p>
-        </article>
-        <article className='movie-spec border'>
           <p>Runtime: {movie.runtime} minutes</p>
           <p>Budget: ${movie.budget}</p>
           <p>Revenue: ${movie.revenue}</p>
+        </article>
+        <article className='movie-info movie-container border'>
+          <h3>{movie.tagline}</h3>
+          <p className='overview'>{movie.overview}</p>
         </article>
       </div>
     </section>
