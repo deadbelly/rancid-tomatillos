715aec47f39d4c1663123895c00a3e7080a2227e
Boone Epstein <booneepstein@gmail.com>
2021-02-07 15:08:46 -0600

    Add styling to home button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,8 @@ import './Header.css'
 const Header = ({selectedDetails, clearSelection}) => {
   return (
     <header>
-      {selectedDetails && <button onClick={clearSelection}>HOME</button>}
       <h1>Rancid<img className='icon' src='./tomatillo.png'/>Tomatillos</h1>
+      {selectedDetails && <button onClick={clearSelection}>HOME</button>}
     </header>
   );
 }
