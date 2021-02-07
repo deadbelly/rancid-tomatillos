7676f9f6fbb0a2a1e2ed45492b4ef507094a03d6
Boone Epstein <booneepstein@gmail.com>
2021-02-06 21:33:19 -0600

    Add dynamic font size adjustment

--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -7,7 +7,7 @@ const Poster = ({id, title, rating, imgSrc, selectMovie}) => {
       <img src={imgSrc}/>
       <div>
         <p>Freshness: {rating.toFixed(2)*10}%</p>
-        <h2>{title}</h2>
+        <h2 className={title.length > 25 && 'long-title'}>{title}</h2>
       </div>
     </div>
   )
