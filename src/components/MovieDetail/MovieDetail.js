646ff42d06dcc76fd20305e965b288fc3fa73a13
Boone Epstein <booneepstein@gmail.com>
2021-02-08 17:22:49 -0600

    Add tabindex to movie detail

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -13,7 +13,7 @@ const MovieDetail = ({movie}) => {
   return (
     <section className='movie-detail-page'>
       <div className='backdrop-img' style={{backgroundImage:`url(${movie.backdrop_path})`}}>
-        <article className='movie-spec movie-container border'>
+        <article className='movie-spec movie-container border' tabIndex='0'>
           <h2 className={movie.title.length >= 20 ? 'long-movie-title' : 'movie-title'}>{movie.title}</h2>
           <p><b>Release Date:</b> {formatDate(movie.release_date)}</p>
           <p><b>Genres:</b> {genres}</p>
@@ -21,7 +21,7 @@ const MovieDetail = ({movie}) => {
           <p><b>Budget:</b> ${new Intl.NumberFormat().format(movie.budget)}</p>
           <p><b>Revenue:</b> ${new Intl.NumberFormat().format(movie.revenue)}</p>
         </article>
-        <article className='movie-info movie-container border'>
+        <article className='movie-info movie-container border' tabIndex='0'>
           <h3>{movie.tagline}</h3>
           <p className='overview'>{movie.overview}</p>
           <p><b>Freshness:</b> {movie.average_rating.toFixed(2)*10}%</p>
