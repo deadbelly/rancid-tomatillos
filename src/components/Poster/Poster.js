e968daec3dbb83ea043c4729ab80770e6444ee27
Boone Epstein <booneepstein@gmail.com>
2021-02-08 16:38:23 -0600

    Change && to ternary

--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -7,7 +7,7 @@ const Poster = ({id, title, rating, imgSrc, selectMovie}) => {
       <img className="poster-img" src={imgSrc}/>
       <div>
         <p>Freshness: {rating.toFixed(2)*10}%</p>
-        <h2 className={title.length > 25 && 'long-title'}>{title}</h2>
+        <h2 className={title.length > 25 ? 'long-title' : ''}>{title}</h2>
       </div>
     </div>
   )
