61ee96567687f3e96cc3ee696c29545d342d1de0
Thao Ma <MaThao@gmail.com>
2021-02-09 16:50:20 -0700

    Add accessibility to header.js component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,9 +5,12 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header>
-      <h1>Rancid<img className='icon' src='./tomatillo.png'/>Tomatillos</h1>
+      <h1>Rancid
+        <img className='icon' src='./tomatillo.png' alt='green tomato icon'/>
+        Tomatillos
+      </h1>
       <Link to='/'>
-        <button>HOME</button>
+        <button alt='home button'>HOME</button>
       </Link>
     </header>
   );
