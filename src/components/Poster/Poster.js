7e5d92f3ac2512005ba6e3e11b25cd7b7b3ef0bb
Thao Ma <MaThao@gmail.com>
2021-02-08 19:21:59 -0700

    Clean up poster codebase

--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -2,17 +2,15 @@ import React from 'react';
 import './Poster.css';
 import { Link } from 'react-router-dom';
 
-const Poster = ({id, title, rating, imgSrc, selectMovie}) => {
+const Poster = ({id, title, rating, imgSrc}) => {
   return (
-    // <div className="poster" tabIndex='0' onClick={() => selectMovie(id)}>
-    <Link  className='poster' to={`/${id}`} onClick={() => selectMovie(id)}>
+    <Link  className='poster' to={`/${id}`}>
       <img className="poster-img" src={imgSrc}/>
       <div>
         <p>Freshness: {rating.toFixed(2)*10}%</p>
         <h2 className={title.length > 25 ? 'long-title' : ''}>{title}</h2>
       </div>
     </Link>
-    // </div>
   )
 }
 
