d015009cc28c02293fca6b9eab4dac5a7d0eabe8
Thao Ma <MaThao@gmail.com>
2021-02-10 21:11:30 -0700

    Add notes

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -8,6 +8,7 @@ describe('App', () => {
     cy.get('header button').should('have.text', 'HOME');
   });
 
+  // this part is called integration testing 
   it('Should be able to click a movie and direct it to a new URL to see movie details and click on the home button to go back', () => {
     cy.contains('Mulan')
       .click()
@@ -39,35 +40,38 @@ describe('MovieDetail', () => {
     cy.get('section').should('have.class', 'movie-detail-page');
   });
 
-  it('Should have a backrop image', () => {
-    cy.get('.movie-detail-page div').should('have.class', 'backdrop-img');
-  });
+  // Here we should do unit testing for each element on the oage 
 
-  it('Should have two info articles', () => {
-    cy.get('article').should('have.length', 2);
-  });
+  // it('Should have a backrop image', () => {
+  //   cy.get('.movie-detail-page div').should('have.class', 'backdrop-img');
+  // });
 
-  it('Should change urls when you click the home button', () => {
-    cy.get('button').click()
-      .get('section').should('have.class', '.poster-grid');
-  })
+  // it('Should have two info articles', () => {
+  //   cy.get('article').should('have.length', 2);
+  // });
+
+  // it('Should change urls when you click the home button', () => {
+  //   cy.get('button').click()
+  //     .get('section').should('have.class', '.poster-grid');
+  // }) this we are already testing so this would be a  duplicate
 });
 
 describe('Error', () => {
   it.skip('Should update the DOM to inform users of a 404', () => {
+    // dont know why but visit needs to go ahead of intercept to work 
+    cy.visit('http://localhost:3000/337401')
     cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/337401', {
       ok: false,
       status: 404,
       statusText: 'Not Found'
     })
-    cy.visit('http://localhost:3000/337401')
-    // .get('.error-status').should('have.text', '404 - Not Found')
-    // .get('.error-expl').should('have.text', 'What you\'re looking for isn\'t here. Double Check the url and try again.');
+    .get('.error-status').should('have.text', '404 - Not Found')
+    .get('.error-expl').should('have.text', 'What you\'re looking for isn\'t here. Double Check the url and try again.');
   });
-  //
-  // it('Should update the DOM to inform users of a 500 error', () => {
-  //   cy.visit('http://localhost:3000/fakepath')
-  //   .get('.error-status').should('have.text', '500 - Internal Server Error')
-  //   .get('.error-expl').should('have.text', 'Something went wrong on our end. Try reloading the page. If the problem persists try back later.');
-  // });
+  
+  it('Should update the DOM to inform users of a 500 error', () => {
+    cy.visit('http://localhost:3000/fakepath')
+    .get('.error-status').should('have.text', '500 - Internal Server Error')
+    .get('.error-expl').should('have.text', 'Something went wrong on our end. Try reloading the page. If the problem persists try back later.');
+  });
 });
