116bca7598a26e8261c1cfe999b21a2ed2650f46
Thao Ma <MaThao@gmail.com>
2021-02-12 13:46:25 -0700

    Add fetch to get videos

--- src/fetchRequests.js
+++ src/fetchRequests.js
@@ -14,6 +14,11 @@ const fetchRequests = {
   getSelectedMovie(id) {
     return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
       .then(response => this.checkResponse(response))
+  },
+
+  getVideos() {
+    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2//movies/${id}/videos`)
+      .then(response => this.checkResponse(response))
   }
 }
 
