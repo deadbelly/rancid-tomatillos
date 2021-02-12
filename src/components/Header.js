6e76d33b3f379d39adf5e8a6702012b0869df70b
Boone Epstein <booneepstein@gmail.com>
2021-02-12 11:55:03 -0600

    Add basic sorting dom

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,26 @@ import React from 'react';
 import '../scss/Header.scss';
 import { Link } from 'react-router-dom';
 
-const Header = ({searchQuery, updateQuery}) => {
+const Header = ({searchQuery, updateQuery, sort}) => {
   return (
     <header>
       <h1>Rancid
         <img className='icon' src='./tomatillo.png' alt='green tomato icon'/>
         Tomatillos
       </h1>
-      <input 
-        type='text' 
-        placeholder='Search Movies By Name' 
+      <input
+        type='text'
+        placeholder='Search Movies By Name'
         aria-label='Search Movies By Name'
         value={searchQuery}
         onChange={ event => updateQuery(event.target.value) }
       />
+      <label>Sort movies by...</label>
+      <select value={sort}>
+        <option value='freshness'>Freshness</option>
+        <option value='date'>Date</option>
+        <option value='name'>Name</option>
+      </select>
       <Link to='/'>
         <button>HOME</button>
       </Link>
