ec0d9b8d2263a1bfaf777d1248c93e7bc4140d46
Boone Epstein <booneepstein@gmail.com>
2021-02-13 15:30:56 -0600

    Add breakpoints for grid

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,22 +9,26 @@ const Header = ({searchQuery, updateQuery, sort, updateSort}) => {
         <img className='icon' src='./tomatillo.png' alt='green tomato icon'/>
         Tomatillos
       </h1>
-      <input
-        type='text'
-        placeholder='Search Movies By Title'
-        aria-label='Search Movies By Title'
-        value={searchQuery}
-        onChange={ event => updateQuery(event.target.value) }
-      />
-      <label>Sort movies by...</label>
-      <select value={sort} onChange={event => updateSort(event.target.value)}>
-        <option value='average_rating'>Freshness</option>
-        <option value='release_date'>Date</option>
-        <option value='title'>Title</option>
-      </select>
       <Link to='/'>
         <button>HOME</button>
       </Link>
+      <form>
+        <input
+          type='text'
+          placeholder='Search Movies By Title'
+          aria-label='Search Movies By Title'
+          value={searchQuery}
+          onChange={ event => updateQuery(event.target.value) }
+        />
+        <div className='sort'>
+          <label htmlFor='sort-options'>Sort movies by </label>
+          <select name='sort-options' value={sort} onChange={event => updateSort(event.target.value)}>
+            <option value='average_rating'>Freshness</option>
+            <option value='release_date'>Date</option>
+            <option value='title'>Title</option>
+          </select>
+        </div>
+      </form>
     </header>
   );
 }
