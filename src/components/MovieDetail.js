f1614bfbb34085796cf2dc73bee8cdc8af011e6b
Boone Epstein <booneepstein@gmail.com>
2021-02-15 18:38:08 -0600

    Refactor detail view to need less code

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -13,26 +13,28 @@ const MovieDetail = ({movie, videos}) => {
 
   return (
     <section>
-      <div className='backdrop-img' style={{backgroundImage:`url(${movie.backdrop_path})`}}>
-        <article className='movie-spec movie-container' tabIndex='0'>
-          <h2 className={movie.title.length >= 20 ? 'long-movie-title' : 'movie-title'}>{movie.title}</h2>
+      <article className='movie-spec movie-container' tabIndex='0'>
+        <h2 className={movie.title.length >= 20 ? 'long-movie-title' : 'movie-title'}>{movie.title}</h2>
+        <img className='backdrop-img' src={movie.backdrop_path} />
+        <h3>{movie.tagline}</h3>
+        <p className='overview'>{movie.overview}</p>
+      </article>
+      <article className='movie-info movie-container' tabIndex='0'>
+        <h2>Stats</h2>
+        <div className='movie-stats'>
+          <p><b>Freshness:</b> {movie.average_rating.toFixed(2)*10}%</p>
           <p><b>Release Date:</b> {date}</p>
           <p><b>Genres:</b> {genres}</p>
           <p><b>Runtime:</b> {movie.runtime} minutes</p>
           <p><b>Budget:</b> ${new Intl.NumberFormat().format(movie.budget)}</p>
           <p><b>Revenue:</b> ${new Intl.NumberFormat().format(movie.revenue)}</p>
-        </article>
-        <article className='movie-info movie-container' tabIndex='0'>
-          <h3>{movie.tagline}</h3>
-          <p className='overview'>{movie.overview}</p>
-          <p><b>Freshness:</b> {movie.average_rating.toFixed(2)*10}%</p>
-        </article>
-        <article className='trailer-container movie-container' tabIndex='0'>
-          <h2>Trailers & Clips</h2>
+        </div>
+        <h2>Trailers & Clips</h2>
+        <div className='trailer-container movie-container' tabIndex='0'>
           {videos.length &&
             videos.map(video => <YouTube videoId={video.key} opts={opts}/> )}
-        </article>
-      </div>
+        </div>
+      </article>
     </section>
   )
 }
